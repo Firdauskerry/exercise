418bd1110f98b2a39a17143ccd05c21308c8cf4c
Firdauskerry <145026971+Firdauskerry@users.noreply.github.com>
2023-09-27 18:17:11 +0300

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,27 @@
+# change 80 to 89 in the list
+student_marks={60,80,90,98}
+student_marks[1] =89
+print(student_marks)
+
+#Add a new item 55(append  new list
+student_marks={60,80,90,98}
+student_marks
+
+
+
+#find the size of the list having appende 55.
+print[len(student_marks)]
+
+#write a python program to sum all the items in the list
+total=sum[student_marks]
+print[total]
+
+#write a python function thst takes two lists and returns two when they have atleast one common member.
+list1=input["enter items 1;"]
+list2=input["enter items 2;"] 
+if 80 in student_marks:
+    print[true,]
+
+
+
+
